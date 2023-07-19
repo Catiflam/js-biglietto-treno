a0b695029fb844a9056d60e13ece8be59532b374
Catiflam <gilfrid1@gmail.com>
2023-07-19 16:13:53 +0200

    edit insert price

--- Js/main.js
+++ Js/main.js
@@ -1,2 +1,15 @@
-const userage = prompt("inserire l'età");
-const userkm = prompt(" inserire quanti km si vuole percorrere");
+//# inserimento input iniziali
+const userage = parseInt(prompt("inserire l'età"));
+const userkm = parseInt(prompt(" inserire quanti km si vuole percorrere"));
+
+let eurokm = 0.21;
+const finalprice = `
+prezzo finale del biglietto
+`;
+
+console.log("prezzo finale dei km percorsi" + eurokm * userkm);
+
+let sconto20 = (eurokm / userkm) * 20;
+let sconto40 = (eurokm / userkm) * 20;
+
+document.getElementById("example").innerHTML = finalprice;
